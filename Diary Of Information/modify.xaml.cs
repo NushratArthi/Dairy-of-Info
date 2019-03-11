2108940f264b20827fe67fa5d0e8f9a667b4a978
NushratArthi <nushrat.arthi@gmail.com>
2019-03-12 04:57:03 +0600

    update query

--- Diary Of Information/modify.xaml.cs
+++ Diary Of Information/modify.xaml.cs
@@ -44,11 +44,10 @@ namespace Diary_Of_Information
             }
 
 
-
             string connectionstring = @"Data Source=ARTHI\ARTHISQL;Initial Catalog=diary;Integrated Security=True";
             SqlConnection sqlcon = new SqlConnection(connectionstring);
 
-            string commandstring = "update table_name set a=@pre,b=@par  where c=xyz";
+            string commandstring = "update information set name=@a,phone=@b,address=@c  where phone='" + txt_phone.Text + "'";
             SqlCommand sqlcmd = new SqlCommand(commandstring, sqlcon);
             sqlcmd.Parameters.Add("@pre", SqlDbType.VarChar).Value = txt_present_address.Text;
             sqlcmd.Parameters.Add("@par", SqlDbType.VarChar).Value = txt_parmanent_address.Text;
