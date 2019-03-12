6b0d3ac395e47e5abba5f98d0d6de904783960ec
nilufa.yesmin567@gmail.com <nilufa.yesmin567@gmail.com>
2019-03-12 00:29:41 -0700

    delete page add

---
+++ Diary Of Information/delete.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Diary_Of_Information
+{
+    /// <summary>
+    /// Interaction logic for delete.xaml
+    /// </summary>
+    public partial class delete : Window
+    {
+        public delete()
+        {
+            InitializeComponent();
+        }
+    }
+}
